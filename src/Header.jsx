4a91d8d5765e359733c6f15d0a269c80dfc1c281
DamirQv <damir119060@gmail.com>
2025-10-02 09:27:12 +0600

    Первый React-проект

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#282c34', padding: '20px', color: 'white' }}>
+      <h2>Мой первый React-проект</h2>
+    </header>
+  )
+}
+
+export default Header
